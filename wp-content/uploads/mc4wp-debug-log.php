e26cbd54dfeb238e3790df574da3d450530bc533
Yoav <yoavxxyoav@gmail.com>
2018-01-02 06:36:17 -0700

    recommitted all the repository with .gitignore git status

--- wp-content/uploads/mc4wp-debug-log.php
+++
@@ -1,3 +0,0 @@
-<?php exit; ?>
-[2017-11-13 20:18:44] WARNING: Form 24892 > yoav******@gm***.com is already subscribed to the selected list(s)
-[2017-11-25 17:07:11] WARNING: Form 24892 > yoav******@gm***.com is already subscribed to the selected list(s)
